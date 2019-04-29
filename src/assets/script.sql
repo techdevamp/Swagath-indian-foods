8d464a17a44dd6c2363f4cb7a344d40cbac65026
SunGuru <sunilbathla14@gmail.com>
2019-04-29 00:44:17 -0400

    ExcelAndSQLFilesAdded

---
+++ src/assets/script.sql
@@ -0,0 +1,18 @@
+
+SELECT * FROM indiangrocerydb.product_item_dtl;
+SELECT * FROM indiangrocerydb.register_user;
+SELECT * FROM indiangrocerydb.file_upload;
+SELECT * FROM indiangrocerydb.product_category_ref;
+SELECT * FROM indiangrocerydb.subscriptions;
+select now();
+/*TRUNCATE TABLE indiangrocerydb.product_item_dtl;
+TRUNCATE TABLE indiangrocerydb.file_upload;
+
+INSERT INTO indiangrocerydb.product_category_ref
+VALUES(1,'WebUser',curdate(),1,'All Grains','Grains',1,NULL);
+INSERT INTO indiangrocerydb.product_category_ref
+VALUES(2,'WebUser',curdate(),1,'All Produce','Produce',1,NULL);
+INSERT INTO indiangrocerydb.product_category_ref
+VALUES(3,'WebUser',curdate(),1,'All Bakery','Bakery',1,NULL);
+INSERT INTO indiangrocerydb.product_category_ref
+VALUES(4,'WebUser',curdate(),1,'All Meat','Meat',1,NULL);*/
