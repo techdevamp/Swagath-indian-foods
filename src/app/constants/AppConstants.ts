f9488ee4ede335c21eef2f4623296166081e2553
SunGuru <sunilbathla14@gmail.com>
2019-07-17 23:13:29 -0400

    Pagination changes

--- src/app/constants/AppConstants.ts
+++ src/app/constants/AppConstants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
     public static get baseURL(): string {
-       return 'https://agile-dawn-98487.herokuapp.com';
-       //return 'http://localhost:8080';
+      return 'https://indiangrocery.herokuapp.com';
+      // return 'http://localhost:8080';
     }
 
   public static get imageURL(): string {
